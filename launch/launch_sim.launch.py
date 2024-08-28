d3f9d96dcf18c2b5dcc6ebe4797f5c7c988c4c0d
Riley Ellis <rileyellis888@gmail.com>
2024-08-28 16:18:20 -0400

    gazebo simulation success

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+#script launches the simlulation
+
+def generate_launch_description():
+    
+    package_name = 'diff_robot'
+
+    #including robot_state_publisher that i made
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
+                launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    #includign gazebo launch file, from gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])
+    )
+
+    #run spawner node from gazebo_ros package. entity name doesn't matter if only single robot
+    spawn_entity = Node(package='gazebo_ros',
+                        executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_robot'],
+                        output='screen')
+    
+    #launch everything
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity
+    ])
